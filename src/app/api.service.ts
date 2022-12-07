c7b08923117c5fbaa6a8c2df0946cd8fb826ba5d
delmashajan <delmashajan@gmail.com>
2022-12-07 15:37:00 +0530

    search

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -15,4 +15,7 @@ export class ApiService {
   {
     return this.http.post("http://localhost:8080/add",dataToSend)
   }
+  searchEmployee=(dataToSend:any)=>{
+    return this.http.post("http://localhost:8080/search",dataToSend)
+  }
 }
